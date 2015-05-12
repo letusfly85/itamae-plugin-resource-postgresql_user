585e326470a4c8cd57f62dd2cb52a0e4db6aac56
letusfly85 <letusfly95@gmail.com>
2015-05-12 22:13:50 +0900

    first commit

---
+++ lib/itamae/plugin/resource/postgresql_user.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/postgresql_user/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module PostgresqlUser
+        # Your code goes here...
+      end
+    end
+  end
+end
